06b42368241031aeeb2f5d61c7c22ce2acf8504b
note-cli <john.medford@note-cli.com>
2024-11-15 08:20:50 -0500

    Update note-cli.rb

--- note-cli.rb
+++ note-cli.rb
@@ -1,26 +1,26 @@
 class Mytool < Formula
-  desc "A simple tool"
-  homepage "https://example.com/mytool"
-  url "https://example.com/path/to/your/executable"
+  desc "note-cli - terminal-based, cloud stored notebook system"
+  homepage "https://example.com/"
+  url "https://example.com/download-latest"
   sha256 "your-sha256-checksum"
 
   depends_on "jansson"
   depends_on "another-dependency"
 
   def install
-    bin.install "executable"
+    bin.install "note-cli"
   end
 
   def post_install
     config_url = "https://example.com/path/to/config/file" # URL to your config file
     home_dir = Dir.home
-    target_file = File.join(home_dir, "config.yaml")
+    target_file = File.join(home_dir, ".noteclirc")
 
     # Download the configuration file
     system "curl", "-o", target_file, config_url
   end
 
   test do
-    system "#{bin}/executable", "--version"
+    system "#{bin}/note-cli", "--version"
   end
 end
