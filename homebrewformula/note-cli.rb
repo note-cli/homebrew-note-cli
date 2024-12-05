e6bef89a1896ef760b2ef8f90471a3054a116dd9
note-cli <john.medford@note-cli.com>
2024-12-05 17:50:45 -0500

    Update note-cli.rb

--- homebrewformula/note-cli.rb
+++ homebrewformula/note-cli.rb
@@ -5,8 +5,6 @@ class NoteCli < Formula
   sha256 "41b27737a54e5b7e1337f8f6ded35433415c6a82ef47e458386c2b6e0b4dca1a"
   version "0.3.7" 
 
-  depends_on "jansson"
-  depends_on "cjson"
   depends_on "curl"
 
   def install
